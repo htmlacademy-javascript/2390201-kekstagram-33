08fe216b2f86283c756c72497a14a26938ecd819
romnosk <romnosk@gmail.com>
2024-10-28 12:05:16 +0300

    Исправляет замечания к ДЗ2

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 // Домашнее задание к разделу 2
 //2.1 Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее.
 const stringNotLongerThanLength = (string, length) => string.length <= length;
+stringNotLongerThanLength('проверяемая строка',20);
 
 // // Тесты функции stringNotLongerLength
 // // Строка короче 20 символов
@@ -13,16 +14,17 @@ const stringNotLongerThanLength = (string, length) => string.length <= length;
 
 // 2.2 Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
 
-let stringIsPalindrome = function (string) {
-  let modifiedString = string.replaceAll(' ','').toUpperCase();
+const stringIsPalindrome = function (string) {
+  const modifiedString = string.replaceAll(' ','').toUpperCase();
   let reversedString = '';
 
-  for (let i = (modifiedString.length-1); i >= 0 ; i -= 1) {
+  for (let i = (modifiedString.length - 1); i >= 0 ; i -= 1) {
     reversedString += modifiedString[i];
   }
   return reversedString === modifiedString;
-}
+};
 
+stringIsPalindrome('топот');
 // // Тесты функции stringIsPalindrome
 // console.log(stringIsPalindrome('А роза упала на лапу Азора'));//true
 // console.log(stringIsPalindrome('не палиндром'));//false
@@ -38,25 +40,25 @@ let stringIsPalindrome = function (string) {
 
 // 2.3 Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
 
-let takeNumeralsFromString = function (string) {
-  string = '' + string;
+const takeNumeralsFromString = function (string) {
+  string = string.toString();
   let numeralsInString = '';
 
-  for (let i = 0; i < string.length; i += 1 ) {
-    switch (string[i]) {
-      case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-        numeralsInString += string[i];
+  for (let i = 0; i < string.length; i += 1) {
+    if (!isNaN(parseInt(string[i], 10))) {
+      numeralsInString += string[i];
     }
   }
   return parseInt (numeralsInString, 10);
-}
+};
 
-// Тесты функции takeNumeralsFromString
-console.log(takeNumeralsFromString('2023 год'));            // 2023
-console.log(takeNumeralsFromString('ECMAScript 2022'));     // 2022
-console.log(takeNumeralsFromString('1 кефир, 0.5 батона')); // 105
-console.log(takeNumeralsFromString('агент 007'));           // 7
-console.log(takeNumeralsFromString('а я томат'));           // NaN
-console.log(takeNumeralsFromString(2023));                  // 2023
-console.log(takeNumeralsFromString(-1));                    // 1
-console.log(takeNumeralsFromString(1.5));                   // 15
+takeNumeralsFromString('2023 год');
+// // Тесты функции takeNumeralsFromString
+// console.log(takeNumeralsFromString('2023 год'));            // 2023
+// console.log(takeNumeralsFromString('ECMAScript 2022'));     // 2022
+// console.log(takeNumeralsFromString('1 кефир, 0.5 батона')); // 105
+// console.log(takeNumeralsFromString('агент 007'));           // 7
+// console.log(takeNumeralsFromString('а я томат'));           // NaN
+// console.log(takeNumeralsFromString(2023));                  // 2023
+// console.log(takeNumeralsFromString(-1));                    // 1
+// console.log(takeNumeralsFromString(1.5));                   // 15
